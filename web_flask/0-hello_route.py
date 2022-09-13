04b324bf65ea967e102acdc8a6960cb6392ff17e
orahpsalm <psalmorahcle@gmail.com>
2022-09-13 21:36:18 +0100

    Create 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Defines message on index page"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """return the message for the index"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
