a57985472c1dcd18b4865c14b8d531c98fe00b8e
Orah, Psalm Chiemeñam <psalmorahcle@gmail.com>
2022-10-16 11:56:28 +0100

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" The module for different pages with Flask framework """
+"""Module for the different pages with Flask"""
 from flask import Flask
 from flask import render_template
 
@@ -9,19 +9,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """ Returns the message for the index """
+    """return the message for the index"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Returns the message for /hbnb """
+    """return the message for /hbnb"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def message_c(text):
-    """ Returns a custom message for c """
+    """custom message for c"""
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
@@ -29,20 +29,20 @@ def message_c(text):
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python', strict_slashes=False)
 def message_python(text='is cool'):
-    """ Returns a custom message for python """
+    """custom message for python"""
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def message_number(n):
-    """ Returns a custom message for /number """
+    """Custom message for /number"""
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def template_number(n):
-    """Function that returns a template"""
+    """Return a template"""
     return render_template('5-number.html', number=n)
 
 
